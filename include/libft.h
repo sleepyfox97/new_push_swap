a90b5bc24aa60f03506a12f3d2ce445583317527
sleepyfox97 <h1649bot@gmail.com>
2021-11-29 23:23:48 +0900

    push ft_lst* and ft_strlen.c

---
+++ include/libft.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khiroshi <khiroshi@student.42tokyo.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 16:40:12 by khiroshi          #+#    #+#             */
+/*   Updated: 2020/11/12 19:33:22 by khiroshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+typedef struct s_dcllist
+{
+	void				*contents;
+	struct s_dcllist	*next;
+	struct s_dcllist	*prev;
+}	t_dcllist;
+
+size_t		ft_strlen(const char *s);
+
+//線形listに関するutil
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list	**lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+
+#endif
